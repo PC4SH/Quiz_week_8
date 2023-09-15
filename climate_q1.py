d53c37289c74a6f317263156b004eefd2619cc63
Fletcher <fletcherbradley123@outlook.com>
2023-09-15 16:11:03 +1000

    Completed Question 1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,29 @@
 import matplotlib.pyplot as plt
-        
+
+# Import Sqlite requirements
+import sqlite3
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+
+# Fetch the stuff
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+
+extract = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
+
+# Iterate through extracted data and add to lists
+for row in extract:
+    year, co2_val, temp_val = row
+    years.append(year)
+    co2.append(co2_val)
+    temp.append(temp_val)
+
+print("Years:", years)
+print("Years:", co2)
+print("Years:", temp)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
